ad4e440ede67dfa4f1cedfdeab563beebd0089d0
franco-nocnoc <franco@nocnoc.app>
2024-04-22 21:45:43 -0300

    sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { QuienSoyComponent } from './components/quien-soy/quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'quien-soy', component: QuienSoyComponent },
+];
